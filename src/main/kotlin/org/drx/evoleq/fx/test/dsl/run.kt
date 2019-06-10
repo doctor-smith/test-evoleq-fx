907b8a29d25b8b9c2adfe65c8ab42e60cb804ec0
Florian Schmidt <florian.schmidt@platis.de>
2019-06-10 23:41:43 +0200

    Slight improvement of fxRunTest and fxPerformTest

--- src/main/kotlin/org/drx/evoleq/fx/test/dsl/run.kt
+++ src/main/kotlin/org/drx/evoleq/fx/test/dsl/run.kt
@@ -21,30 +21,31 @@ import org.drx.evoleq.evolving.Evolving
 import org.drx.evoleq.fx.application.BgAppManager
 import org.drx.evoleq.test.TestData
 import org.drx.evoleq.test.testRunner
-import org.drx.evoleq.time.Change
+import org.drx.evoleq.time.change
 import org.drx.evoleq.time.happen
 import org.testfx.api.FxToolkit
 
 
-suspend fun fxPerformTest(timeout: Long = 10_000,test: suspend CoroutineScope.()->Unit): Evolving<suspend CoroutineScope.() -> Unit> {
-    val change = Change(test)
+suspend fun fxPerformTest(timeout: Long = 10_000, test: suspend CoroutineScope.()->Unit): Evolving<suspend CoroutineScope.() -> Unit> {
+    val change = GlobalScope.change(test)
     val changing = change.happen()
-    testRunner.send(TestData(timeout,change))
+    testRunner().send(TestData(timeout,change))
     return changing
 }
 
-
+var m: Application? = null
 fun fxRunTest(timeout: Long = 10_000, test: suspend CoroutineScope.()->Unit): Unit = runBlocking {
     var m: Application? = null
-
+    if(m == null) {
         FxToolkit.registerPrimaryStage()
         m = FxToolkit.setupApplication { BgAppManager() }
-        var validate: suspend CoroutineScope.()->Unit = {}
-        try {
-            validate = fxPerformTest(timeout){test()}.get()
-        }finally {
-            FxToolkit.cleanupApplication(m!!)
-            FxToolkit.cleanupStages()
-        }
-        validate()
+    }
+    var validate: suspend CoroutineScope.()->Unit = {}
+    try {
+        validate = fxPerformTest(timeout){test()}.get()
+    }finally {
+        //FxToolkit.cleanupApplication(m!!)
+        FxToolkit.cleanupStages()
+    }
+    validate()
 }
